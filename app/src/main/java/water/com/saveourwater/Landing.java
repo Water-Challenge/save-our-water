d8d2b0ee8851cffdac39fd2d1cce7f9b205d8cb9
Tania Gandhe <tania@brit.co>
2015-06-06 17:58:51 -0700

    added functionality for take photo and upload photo

--- app/src/main/java/water/com/saveourwater/Landing.java
+++ app/src/main/java/water/com/saveourwater/Landing.java
@@ -1,17 +1,73 @@
 package water.com.saveourwater;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class Landing extends ActionBarActivity {
+
+    int UPLOAD_PHOTO = 1;
+    int TAKE_PHOTO = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        Button take_photo = (Button) findViewById(R.id.btTakePhoto);
+        Button upload_photo = (Button) findViewById(R.id.btUploadPhoto);
+        take_photo.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                takePhoto(TAKE_PHOTO);
+            }
+        });
+
+        upload_photo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openGallery(UPLOAD_PHOTO);
+            }
+        });
+    }
+
+    public void takePhoto(int req_code){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    public void openGallery(int req_code){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select file to upload "), req_code);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_PHOTO && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+            Log.e("TANIA", "taken photo !!!"+ selectedImageUri);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        }
+
+        if (requestCode == UPLOAD_PHOTO && resultCode == RESULT_OK) {
+
+            Uri selectedImageUri = data.getData();
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+        } else if (resultCode == RESULT_CANCELED) {
+            // Handle cancel
+        }
     }
 
     @Override
